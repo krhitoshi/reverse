141fa2d3ebd49d6702822f104378d31d5ac1474c
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2013-12-13 22:52:54 +0900

    basic functions

--- lib/reverse.rb
+++ lib/reverse.rb
@@ -1,5 +1,40 @@
 require "reverse/version"
 
-module Reverse
-  # Your code goes here...
+require 'socket'
+
+class Reverse
+  ADDR_REG_EXP = /\b((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))\b/
+
+  def initialize
+    @list = {}
+  end
+
+  def exec
+    while line = gets
+      if line =~ ADDR_REG_EXP
+        res = line.gsub(ADDR_REG_EXP) do |addr|
+          name = nil
+          if @list[addr]
+            name = @list[addr]
+          else
+            name = get_name(addr)
+            @list[addr] = name
+          end
+          name
+        end
+        print res
+      end
+    end
+  end
+
+  private
+
+  def get_name(addr)
+    info = Socket.getnameinfo(Socket.sockaddr_in('80', addr))
+    info[0]
+  end
+
 end
+
+
+
--- lib/reverse/version.rb
+++ lib/reverse/version.rb
@@ -1,3 +1,3 @@
-module Reverse
+class Reverse
   VERSION = "0.0.1"
 end
