09b785edfc9ff0b74f38b346672a356102f3d352
Hitoshi Kurokawa <hitoshi@nextseed.jp>
2014-01-12 20:57:47 +0900

    use resolv.rb

--- lib/reverse.rb
+++ lib/reverse.rb
@@ -1,11 +1,13 @@
 require "reverse/version"
 require "socket"
 require "term/ansicolor"
+require "resolv"
 
 class Reverse
   ADDR_REG_EXP = /\b((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))\b/
 
   def initialize
+    @resolv = Resolv::DNS.new(nameserver: ["8.8.8.8", "8.8.4.4"], ndots: 1)
     @list = {}
     Signal.trap(:INT){ exit(0) }
   end
@@ -39,8 +41,11 @@ class Reverse
   end
 
   def get_name(addr)
-    info = Socket.getnameinfo(Socket.sockaddr_in("80", addr))
-    info[0]
+    begin
+      @resolv.getname(addr).to_s
+    rescue Resolv::ResolvError => e
+      addr
+    end
   end
 
 end
